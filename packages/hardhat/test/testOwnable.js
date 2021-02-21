0d48639b6702bed388393496fea06d6513e2ae2b
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2021-02-21 09:34:19 -0800

    Working on tests.

--- packages/hardhat/test/testOwnable.js
+++ packages/hardhat/test/testOwnable.js
@@ -1,25 +1,49 @@
-const { ethers } = require('hardhat');
-const { use, expect } = require('chai');
-const { solidity } = require('ethereum-waffle');
+const { ethers } = require("hardhat");
+const { use, expect } = require("chai");
+const { solidity } = require("ethereum-waffle");
 
 use(solidity);
 
-describe('My Dapp', function() {
+describe("My Dapp", function () {
   let myContract;
+  let addresslist;
 
-  describe("Ownable", function() {
-    
-    it('Should deploy Ownable.', async function () {
-      const Ownable = await ethers.getContractFactory('Ownable');
-      myContract = await Ownable.deploy();
-    });
+  beforeEach(async function() {
+    addressList = await ethers.getSigners();
+    const Ownable = await ethers.getContractFactory("Ownable");
+    myContract = await Ownable.deploy();
+  });
+
+  describe("Ownable", function () {
 
     it("Should know I'm the owner.", async function () {
-      const Ownable = await ethers.getContractFactory('Ownable');
-      myContract = await Ownable.deploy();
       expect(await myContract.amIOwner()).to.equal(true);
     });
 
+    it("Should know they're not the owner.", async function () {
+      expect(await myContract.connect(addressList[1]).amIOwner()).to.equal(
+        false
+      );
+    });
 
-  })
-})
+    it("Should cancel prior ownership when transfering.", async function () {
+      await myContract.transferOwnership(addressList[1].address);
+      expect(await myContract.amIOwner()).to.equal(false);
+    });
+
+    it("Should aquire new ownership when transferring", async function () {
+      await myContract.transferOwnership(addressList[1].address);
+      expect(
+        await myContract.connect(addressList[1].address).amIOwner()
+      ).to.equal(true);
+    });
+
+    it("Should not transfer if non-owner requests transfer.", async function () {
+      expect(
+        await myContract
+          .connect(addressList[1].address)
+          .transferOwnership(addressList[2].address)
+      ).to.throw();
+    });
+  });
+});
