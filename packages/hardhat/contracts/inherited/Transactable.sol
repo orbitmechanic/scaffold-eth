7c8aad606cf0b7f6a4f848994f443145e4aa8adb
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2021-02-27 19:02:26 -0800

    Accountable -> Transactable

---
+++ packages/hardhat/contracts/inherited/Transactable.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.8.0;
+import './Pausable.sol';
+
+// SPDX-License-Identifier: UNLICENSED
+
+// Handles raw ETH balance of a contract.
+contract Transactable is Pausable {
+    
+    event receipt(
+        address recipiant, 
+        uint256 amount);
+    
+    modifier costs(uint256 cost){
+        require(msg.value >= cost, 'Insufficient funds sent.');
+        _;
+    }
+
+    // Absorb ETH
+    receive() external payable Unpaused {
+        emit receipt(msg.sender, msg.value);
+    }
+    
+    // Safely release ETH
+    function send(address payable to, uint256 amount) internal Unpaused {
+        require(amount <= address(this).balance, 'Insufficient funds available.');
+        to.transfer(amount);  // reverts on fail.
+        emit receipt(to, amount);
+    }
+}
---
+++ packages/hardhat/test/testTransactable.js
@@ -0,0 +1,41 @@
+const { ethers } = require("hardhat");
+const { use, expect } = require("chai");
+const { solidity } = require("ethereum-waffle");
+
+use(solidity);
+
+describe("Testing", function () {
+  let myContract;
+
+  beforeEach(async function () {
+    const contractFactory = await ethers.getContractFactory("Accountable");
+    myContract = await contractFactory.deploy();
+  });
+
+  describe("Accountable", function () {
+    it("Should have a zero balance to start.", async function () {
+      expect(await myContract.contractBalance()).to.equal(0);
+    });
+
+    it("Should absorb 1.2 stray eth.", async function () {
+      const addressList = await ethers.getSigners();
+      // <Transmit 1.2 ETH from addressList[1] to myContract.deployedAddress();>
+      expect(myContract.contractBalance()).to.equal(1.2);
+    });
+
+    it("Should emitt a recipt when paid.", async function () {});
+      const addressList = await ethers.getSigners();
+      expect( 
+        <Transmit 1.2 ETH 
+          from addressList[1] 
+          to myContract.deployedAddress();>)
+          .to.emit(myContract.receipt)
+          .withArgs(addressList[1],1.2);
+    });
+
+    it("Should not send for externals.", async function () {
+      const addressList = await ethers.getSigners();
+      await expect(myContract.debt(addressList[1], 200)).to.be.reverted;
+    });
+  });
+});
